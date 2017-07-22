abf07df9ea5505f0ecd0f631e3d5e89959e5ec39
Edwin Moses <workockmoses@gmail.com>
2017-07-22 11:34:18 +0300

    initiated server on port 3000

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(3000, '127.0.0.1', () => {
+  console.log("Server listening on port 3000!");
+});
