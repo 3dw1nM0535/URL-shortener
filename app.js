24148e7fae3d30e42eee324f194e9826b3b78849
Edwin Moses <workockmoses@gmail.com>
2017-07-22 14:19:16 +0300

    mongo connect

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var mongo = require('mongodb').MongoClient;
-var valiUrl =  require('valid-url');
+var validUrl =  require('valid-url');
 var shortId = require('shortid');
+var port = process.env.PORT || 3000;
+var dbUrl = "mongodb://url-shorter:@03167TuK2014@ds031597.mlab.com:31597/url-shortener";
 
 var app = express();
 
@@ -12,11 +14,45 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + "index.html");
 });
 
-app.get('/:url(*)', (req, res) => {
+app.get('/new/:url(*)', (req, res) => {
   var url = req.params.url;
-  if (url.)
+  if (validUrl.isUri(url)) {
+    mongo.connect(dbUrl, (err, db) => {
+      if(err) {
+        res.send("Connection to the database was lost! Please try later");
+        return console.log(err);
+      } else {
+        var urlList = db.collection('urls');
+        var short = shortId.generate();
+        urlList.insert({url: url, short: short}, () => {
+          var data = {
+            originalUrl: url,
+            shortUrl: 'http://' + req.headers['host'] + '/' + short,
+          }
+          db.close();
+          res.send(data);
+        });
+      }
+    });
+  } else {
+    var data = {
+      error: 'Please check your input for mistakes',
+    }
+    res.json(data);
+  }
 });
 
-app.listen(3000, '127.0.0.1', () => {
+app.get('/:value', (req, res) => {
+  var value = req.params.value;
+  mongo.connect(dbUrl, (err, db) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      
+    }
+  });
+});
+
+app.listen(port, '127.0.0.1', () => {
   console.log("Server listening on port 3000!");
 });
